3991aec7dad4928743e0459629fc03f793498d8d
SaeedRahmanRao <raosaeedrahmankhan@gmail.com>
2024-08-21 21:33:35 +0500

    saeed

---
+++ components/AboutSection.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import Image from "next/image";
+import { AiOutlineYoutube } from "react-icons/ai";
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "React.js" },
+  { skill: "Next.js" },
+  { skill: "React-Native" },
+  { skill: "Python" },
+  { skill: "Streamlit" },
+  { skill: "Generative AI" },
+  { skill: "C++" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+];
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Saeed and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> Web Developer
+            </p>
+            <br />
+            <p>
+              I am an undergraduate Computer Science student at the University
+              of Central Punjab in Lahore, Pakistan. My academic journey here
+              has been enriching, providing me with a strong foundation in
+              computer science principles and practical experience in various
+              programming and development areas
+            </p>
+            <br />
+            <p>
+              My proficiency in multiple programming languages and hands-on
+              experience with web development tools like Next.js have further
+              enhanced my technical capabilities. I am passionate about
+              exploring emerging technologies and applying my knowledge to solve
+              real-world problems, continually seeking new challenges and
+              learning opportunities.
+            </p>
+            <br />
+            <div className="flex">
+              <p>
+                In addition to my academic journey, I have taken the initiative
+                to share my knowledge by teaching Design and Analysis of
+                Algorithms and Operating Systems (Ubuntu) on my YouTube channel,
+                "Reviving Skills." This experience has allowed me to deepen my
+                understanding of these subjects while helping others grasp
+                complex concepts. Creating content for the channel has been a
+                rewarding way to reinforce my learning and contribute to the
+                broader tech community.{" "}
+                <a
+                  href="https://youtube.com/@revivingskills?si=xw0iAPjpfRKE5N5h"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <AiOutlineYoutube
+                    className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                    size={30}
+                  />
+                </a>
+              </p>
+            </div>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+            <Image
+              src="/avatar.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-35 md:z-0 md:top-20"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
+
+const projects = [
+  {
+    name: "Web-Based Social Media Application",
+    description:
+      "This social media app offers Google account login for easy access. Users can create, like, and dislike posts, fostering meaningful interactions. Logging out is a simple process, prioritizing privacy and user experience.Project Github repo in unavaiable.I will update it soon",
+    image: "/reactappp.png",
+    github: "",
+    link: "https://first-project-db276.firebaseapp.com/",
+  },
+  {
+    name: "Issue Tracker",
+    description:
+      "Developed a full-stack issue tracker using Next.js, featuring user authentication with NextAuth, and database management with Prisma and MySQL. Implemented core functionalities including issue creation, updating, viewing, and deletion. Enhanced user experience with advanced features such as sorting, filtering, and editing issues. Also integrated graphical data representation for tracking progress. Leveraged multiple libraries to optimize performance and scalability. Deployment is currently under maintenance, and I will update once it is live",
+    image: "/nextapp.png",
+    github: "https://github.com/SaeedRahmanRao/issue-tracker",
+    link: "",
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" mt-10 md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
